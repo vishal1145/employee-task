942502a9e6326a2bfd0283f3de98f8d1e2d6cdd0
vishal <vishal.kumar1145@gmail.com>
2024-04-08 14:05:05 +0530

    be config

--- be/database/config.js
+++ be/database/config.js
@@ -1,10 +1,10 @@
 const mongoose= require('mongoose');
 // mongoose.connect("mongodb://0.0.0.0:27017/empstatus");
 
-mongoose.connect(
-    // "mongodb+srv://yashgupta365ji:Y@sh7247@algofolksdb.cp3amtq.mongodb.net/empstatus"
-    "mongodb+srv://yashgupta7247:Y%40sh7247@cluster0.4pumz8s.mongodb.net/empstatus"
-);
+// mongoose.connect(
+    // "mongodb+srv://yashgupta365ji:Y%40sh7247@algofolksdb.cp3amtq.mongodb.net/empstatus"
+    //"mongodb+srv://yashgupta7247:Y%40sh7247@cluster0.4pumz8s.mongodb.net/empstatus"
+// );
 
 // ***********for mysql*****************
 // const mysql= require('mysql');
@@ -15,3 +15,14 @@ mongoose.connect(
 //     database:'college'
 // });
 // module.exports=conn;
+
+const uri =
+  "mongodb+srv://yashgupta365ji:Y%40sh7247@algofolksdb.cp3amtq.mongodb.net/empstatus";
+mongoose.connect(uri, {
+  // Increase the socket timeout to 30 seconds
+  socketTimeoutMS: 30000,
+  // Increase the connection timeout to 30 seconds
+  connectTimeoutMS: 30000,
+  // Increase the server selection timeout to 30 seconds
+  serverSelectionTimeoutMS: 30000,
+});
